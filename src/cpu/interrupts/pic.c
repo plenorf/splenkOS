ad6c2f0022310b9bd7a58f34414a4d220e10b8fd
plenorf <plenorf@gmail.com>
2025-08-10 09:05:14 +1000

    I did the pic thing hopefully it works

--- src/cpu/interrupts/pic.c
+++ src/cpu/interrupts/pic.c
@@ -1,2 +1,90 @@
 #include "pic.h"
 #include "../io.h"
+
+void PIC_sendEOI(uint8_t irq)
+{
+    if (irq >= 8) {
+        outb(PIC2_COMMAND, PIC_EOI);
+    }
+    outb(PIC1_COMMAND, PIC_EOI);
+}
+
+// Initialization dont ask me how it works spook idk how much of this works
+
+void PIC_remap(int offset1, int offset2)
+{
+    outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
+    io_wait();
+    outb(PIC1_DATA, offset1);
+    io_wait();
+    outb(PIC2_DATA, offset2);
+    io_wait();
+    outb(PIC1_DATA, 4);
+    io_wait();
+    outb(PIC2_DATA, 2);
+    io_wait();
+    //This is a lot of waiting
+
+    outb(PIC1_DATA, ICW4_8086);
+    io_wait();
+    outb(PIC2_DATA, ICW4_8086);
+    io_wait();
+
+    outb(PIC1_DATA, 0);
+    outb(PIC2_DATA, 0);
+}
+
+// Disabling the PIC cause I was told to do so
+
+void pic_disable(void) {
+    outb(PIC1_DATA, 0xFF);
+    outb(PIC2_DATA, 0xFF);
+}
+//Something about Masking idk
+
+void IRQ_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if (IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) | (1 << IRQline);
+    outb(port, value);
+}
+void IRQ_clear_mask(uint8_t IRQline) {
+    uint16_t port;
+    uint8_t value;
+
+    if (IRQline < 8) {
+        port = PIC1_DATA;
+    } else {
+        port = PIC2_DATA;
+        IRQline -= 8;
+    }
+    value = inb(port) & ~(1 << IRQline);
+    outb(port, value);
+}
+
+//Lets us know which interrupts are getting served and which are raised
+static uint16_t __pic_get_irq_reg(int ocw3)
+{
+    outb(PIC1_COMMAND, ocw3);
+    outb(PIC2_COMMAND, ocw3);
+    return (inb(PIC2_COMMAND) << 8) | inb(PIC1_COMMAND);
+}
+
+uint16_t PIC_get_irr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_IRR);
+}
+
+uint16_t pic_get_isr(void)
+{
+    return __pic_get_irq_reg(PIC_READ_ISR);
+}
--- src/cpu/interrupts/pic.h
+++ src/cpu/interrupts/pic.h
@@ -1,1 +1,24 @@
+//Idk why we need so many definitions but hey
+
 #pragma once
+#define PIC1    0x20
+#define PIC2    0xA0
+#define PIC1_COMMAND PIC1
+#define PIC1_DATA  (PIC1 + 1)
+#define PIC2_COMMAND PIC2
+#define PIC2_DATA  (PIC2 + 1)
+#define PIC_EOI 0x20
+
+#define ICW1_ICW4 0x01
+#define IWC1_SINGLE 0x02
+#define ICW1_INTERVAL_4 0x04
+#define ICW1_LEVEL 0x08
+#define ICW1_INIT 0x10
+
+#define ICW4_8086 0x01
+#define ICW4_AUTO 0x02
+#define ICW4_BUF_SLAVE 0x08
+#define ICW4_BUF_MASTER 0x0C
+
+#define PIC_READ_IRR 0x0a 
+#define PIC_READ_ISR 0x0b
