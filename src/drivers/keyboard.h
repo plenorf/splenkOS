74a178c73d9dfe57bb189d2f6cb078a4d621fc7d
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-08-10 19:50:02 +1000

    read_char is pretty much done!

--- src/drivers/keyboard.h
+++ src/drivers/keyboard.h
@@ -11,6 +11,7 @@ void keyboard_handler();
 
 char *keycode = "\e 1234567890-=\b\tqwertyuiop[]\n\0asdfghjkl;'`\0\\zxcvbnm,./\0\0\0 ";
 char *keycode_shift = "\e !@#$%^&*()_+\b\tQWERTYUIOP{}\n\0ASDFGHJKL:\"~\0|ZXCVBNM<>?\0\0\0 ";
+bool isShift = false;
 
 void keyboard_interrupt_handler(InterruptFrame* frame) {
     
@@ -29,21 +30,29 @@ int keyboard_init() {
 
 char read_char()
 {
-    int run = 1;
+    int run = true;
     char out = 0;
+
     while (run)
     {
+
         if (inb(0x64) & 0x1)
         {
-            if (inb(0x60) == 0x2A && !isShift)
-                isShift = 1;
-            else if (inb(0x60) == 0xAA && isShift)
-                isShift = 0;
+            uint8_t scancode = inb(0x60);
+            if (scancode == 0x2A && !isShift)
+                isShift = true;
+            else if (scancode == 0xAA && isShift)
+                isShift = false;
             if (!isShift)
-                out = keycode[inb(0x60)];
-            
+                out = keycode[scancode];
+            else if (isShift)
+                out = keycode_shift[scancode];
+            if (out != 0 && scancode < 60)
+                run = false;
         }
     }
+    
+    return out;
 }
 
 Driver keyboardDriver = {
