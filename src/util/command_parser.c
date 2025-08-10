fd6d0891da3dd68126941d0b9626ef1c3dfc2016
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-08-11 05:47:51 +1000

    added note comment to command_parser.c

--- src/util/command_parser.c
+++ src/util/command_parser.c
@@ -1,6 +1,10 @@
 #include "command_parser.h"
 #include "../libc/string.h"
 
+// this is terrible coding right here but it'll be removed later.
+// its gonna be mainly used for testing features of the OS without
+// having to hardcode exact values, which will make things easier
+
 void parseCommand(char *input) {
 	char *command = strtok(input, " ");
 	if (command) {
