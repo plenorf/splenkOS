1aa07eec9e582a23b3e10a32898b3cbd7e2292b4
SpookyDervish <78246495+SpookyDervish@users.noreply.github.com>
2025-08-10 19:34:09 +1000

    defined isShift

--- src/drivers/keyboard.h
+++ src/drivers/keyboard.h
@@ -11,6 +11,7 @@ void keyboard_handler();
 
 char *keycode = "\e 1234567890-=\b\tqwertyuiop[]\n\0asdfghjkl;'`\0\\zxcvbnm,./\0\0\0 ";
 char *keycode_shift = "\e !@#$%^&*()_+\b\tQWERTYUIOP{}\n\0ASDFGHJKL:\"~\0|ZXCVBNM<>?\0\0\0 ";
+int isShift = 0;
 
 void keyboard_interrupt_handler(InterruptFrame* frame) {
     
