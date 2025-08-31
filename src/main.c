ec067d01ee33deccce721d380e743b00cc0d5878
plenorf <plenorf@gmail.com>
2025-08-31 18:34:05 +1000

    More Ram dectect stuff

--- src/main.c
+++ src/main.c
@@ -47,10 +47,11 @@ static volatile struct limine_framebuffer_request framebuffer_request = {
 // Also this can be moved to any .c file as seen fit if we includ main.h or where ever the linine stuff is from
 
 __attribute__((used, section(".limine_requests")))
-static volatile struct limine_memmap_request memmap_request = {
+volatile struct limine_memmap_request memmap_request = {
     .id = LIMINE_MEMMAP_REQUEST,
     .revision = 0
 };
+
 
 
 // Finally, define the start and end markers for the Limine requests.
